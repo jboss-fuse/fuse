074bd87878982df1d3c30dd6c41fc630c64d1b2b
Dhiraj Bokde <dbokde@redhat.com>
2015-02-15 04:21:34 -0800

    ENTESB-2381, fixed camel linkedin quickstart

---
+++ quickstarts/camel/camel-linkedin/src/main/java/org/jboss/quickstarts/fuse/processor/BodyInAggregatingStrategy.java
@@ -0,0 +1,47 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright 2014, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.quickstarts.fuse.processor;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Property;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
+
+public class BodyInAggregatingStrategy implements AggregationStrategy {
+
+    public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+        if (oldExchange == null) {
+            return newExchange;
+        }
+
+        String oldBody = oldExchange.getIn().getBody(String.class);
+        String newBody = newExchange.getIn().getBody(String.class);
+        oldExchange.getIn().setBody(oldBody + "+" + newBody);
+        return oldExchange;
+    }
+
+    /**
+     * An expression used to determine if the aggregation is complete
+     */
+    public boolean isCompleted(@Property(Exchange.AGGREGATED_SIZE) Integer aggregated) {
+        if (aggregated == null) {
+            return false;
+        }
+
+        return aggregated == 3;
+    }
+
+}
