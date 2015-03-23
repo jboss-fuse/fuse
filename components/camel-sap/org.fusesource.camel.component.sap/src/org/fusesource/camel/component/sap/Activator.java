13a4d4057cc8b201e07b462c6f662435d64d7b63
William Collins <punkhornsw@gmail.com>
2015-03-23 14:14:13 -0400

    [ENTESB-2910] Added code to bundle activator to unregister JCo3 Data Providers.

--- components/camel-sap/org.fusesource.camel.component.sap/src/org/fusesource/camel/component/sap/Activator.java
+++ components/camel-sap/org.fusesource.camel.component.sap/src/org/fusesource/camel/component/sap/Activator.java
@@ -16,8 +16,12 @@
  */
 package org.fusesource.camel.component.sap;
 
+import org.fusesource.camel.component.sap.util.ComponentDestinationDataProvider;
+import org.fusesource.camel.component.sap.util.ComponentServerDataProvider;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+
+import com.sap.conn.jco.ext.Environment;
 
 public class Activator implements BundleActivator {
 
@@ -40,6 +44,10 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
+		// Ensure data providers are unregistered s.t. this bundle can be reinstalled.
+		Environment.unregisterDestinationDataProvider(ComponentDestinationDataProvider.INSTANCE);
+		Environment.unregisterServerDataProvider(ComponentServerDataProvider.INSTANCE);
+
 		Activator.context = null;
 	}
 
