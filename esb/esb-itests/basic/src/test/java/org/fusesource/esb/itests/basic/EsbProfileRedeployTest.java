8fc887af3004050167b890bdd5c1c25c96d58e14
Kevin Earls <kevin@kevinearls.com>
2015-04-02 16:49:15 +0200

    ENTESB-1792 Fixed EsbProfileRedeployTest

--- esb/esb-itests/basic/src/test/java/org/fusesource/esb/itests/basic/EsbProfileRedeployTest.java
+++ esb/esb-itests/basic/src/test/java/org/fusesource/esb/itests/basic/EsbProfileRedeployTest.java
@@ -20,18 +20,21 @@ import io.fabric8.api.Container;
 import io.fabric8.api.FabricService;
 import io.fabric8.api.ServiceProxy;
 import io.fabric8.groups.MultiGroup;
+import io.fabric8.groups.NodeState;
 import io.fabric8.groups.internal.ManagedGroupFactory;
 import io.fabric8.groups.internal.ManagedGroupFactoryBuilder;
+import io.fabric8.groups.internal.ZooKeeperMultiGroup;
+import io.fabric8.groups.internal.ZooKeeperMultiGroup;
 import io.fabric8.itests.paxexam.support.ContainerBuilder;
 import io.fabric8.itests.paxexam.support.ContainerProxy;
-import io.fabric8.itests.paxexam.support.FabricTestSupport;
 import io.fabric8.itests.paxexam.support.Provision;
 import io.fabric8.mq.fabric.discovery.FabricDiscoveryAgent;
 import org.apache.curator.framework.CuratorFramework;
-import org.junit.Ignore;
+import org.fusesource.esb.itests.pax.exam.karaf.EsbTestSupport;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.MavenUtils;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.options.DefaultCompositeOption;
@@ -39,21 +42,21 @@ import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerMethod;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.Callable;
 
-import static org.fusesource.esb.itests.pax.exam.karaf.EsbTestSupport.executeCommandAsAdmin;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
+import static org.ops4j.pax.exam.CoreOptions.wrappedBundle;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
 
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerMethod.class)
-public class EsbProfileRedeployTest extends FabricTestSupport {
+public class EsbProfileRedeployTest extends EsbTestSupport {
 
-    private long timeout = 300 * 1000L;
+    private long timeout = 30 * 1000L;
 
-    @Ignore("ENTESB-1792")
     @Test
     public void testProfileRedeploy() throws Exception {
         executeCommandAsAdmin("fabric:create -n");
@@ -61,7 +64,8 @@ public class EsbProfileRedeployTest extends FabricTestSupport {
         try {
             FabricService fabricService = fabricProxy.getService();
 
-            Set<ContainerProxy> containers = ContainerBuilder.create(fabricProxy, 1).withJvmOpts("-Xmx512m -XX:MaxPermSize=128m").withName("node").withProfiles("jboss-fuse-full").assertProvisioningResult().build();
+            Set<ContainerProxy> containers = ContainerBuilder.create(fabricProxy, 1).withJvmOpts("-Xmx512m -XX:MaxPermSize=128m")
+                    .withName("node").withProfiles("jboss-fuse-full").assertProvisioningResult().build();
             try {
                 Container node = containers.iterator().next();
                 Provision.provisioningSuccess(Arrays.asList(node), PROVISION_TIMEOUT);
@@ -73,13 +77,14 @@ public class EsbProfileRedeployTest extends FabricTestSupport {
                         throw new Exception("Shouldn't be called");
                     }
                 });
-                final MultiGroup group = (MultiGroup) factory.createMultiGroup("/fabric/registry/clusters/fusemq/default", FabricDiscoveryAgent.ActiveMQNode.class);
+                final MultiGroup group = (MultiGroup) factory.createMultiGroup("/fabric/registry/clusters/amq/default", FabricDiscoveryAgent.ActiveMQNode.class);
+                ZooKeeperMultiGroup wtf =(ZooKeeperMultiGroup) group;  // TODO remove?
                 group.start();
                 FabricDiscoveryAgent.ActiveMQNode master = null;
                 Provision.waitForCondition(new Callable<Boolean>() {
                     @Override
                     public Boolean call() throws Exception {
-                        while ((FabricDiscoveryAgent.ActiveMQNode) group.master() == null) {
+                        while (group.slaves().isEmpty()) {
                             Thread.sleep(1000);
                         }
                         return true;
@@ -88,36 +93,38 @@ public class EsbProfileRedeployTest extends FabricTestSupport {
 
                 master = (FabricDiscoveryAgent.ActiveMQNode) group.master();
                 String masterContainer = master.getContainer();
-                assertEquals("node1", masterContainer);
+                assertEquals("root", masterContainer);
+                assertTrue(getSlaveNames(group).contains("node"));
 
                 for (int i = 0; i < 5; i++) {
 
                     Thread.sleep(5000);
 
-                    executeCommandAsAdmin("container-remove-profile node1 jboss-fuse-full");
+                    executeCommandAsAdmin("container-remove-profile node jboss-fuse-full");
                     Provision.provisioningSuccess(Arrays.asList(fabricService.getContainers()), PROVISION_TIMEOUT);
 
                     Provision.waitForCondition(new Callable<Boolean>() {
                         @Override
                         public Boolean call() throws Exception {
-                            while ((FabricDiscoveryAgent.ActiveMQNode) group.master() != null) {
+                            while (!group.slaves().isEmpty()) {
                                 Thread.sleep(1000);
                             }
                             return true;
                         }
                     }, timeout);
                     master = (FabricDiscoveryAgent.ActiveMQNode) group.master();
-                    assertNull(master);
+                    assertEquals("root", master.getContainer());
+                    assertFalse(getSlaveNames(group).contains("node"));
 
                     Thread.sleep(5000);
 
-                    executeCommandAsAdmin("container-add-profile node1 jboss-fuse-full");
+                    executeCommandAsAdmin("container-add-profile node jboss-fuse-full");
                     Provision.provisioningSuccess(Arrays.asList(fabricService.getContainers()), PROVISION_TIMEOUT);
 
                     Provision.waitForCondition(new Callable<Boolean>() {
                         @Override
                         public Boolean call() throws Exception {
-                            while ((FabricDiscoveryAgent.ActiveMQNode) group.master() == null) {
+                            while (group.slaves().isEmpty()) {
                                 Thread.sleep(1000);
                             }
                             return true;
@@ -126,8 +133,8 @@ public class EsbProfileRedeployTest extends FabricTestSupport {
 
                     master = (FabricDiscoveryAgent.ActiveMQNode) group.master();
                     masterContainer = master.getContainer();
-                    assertEquals("node1", masterContainer);
-
+                    assertEquals("root", masterContainer);
+                    assertTrue(getSlaveNames(group).contains("node"));
                 }
             } finally {
                 ContainerBuilder.destroy(containers);
@@ -137,11 +144,24 @@ public class EsbProfileRedeployTest extends FabricTestSupport {
         }
     }
 
+    private List<String> getSlaveNames(MultiGroup group) {
+        List<String> slaves = new LinkedList<>();
+        for (NodeState state : (List<NodeState>) group.slaves()) {
+            slaves.add(state.getContainer());
+        }
+
+        return slaves;
+    }
+
     @Configuration
    	public Option[] config() {
    		return new Option[]{
-   				new DefaultCompositeOption(fabricDistributionConfiguration()),
-                features("default", "mq-fabric")
+                new DefaultCompositeOption(esbDistributionConfiguration("jboss-fuse-full")),
+                features("default", "mq-fabric"),
+                mavenBundle("io.fabric8.itests.paxexam", "fabric-itests-paxexam-common", MavenUtils.getArtifactVersion("io.fabric8.itests.paxexam", "fabric-itests-paxexam-common")),
+                mavenBundle("io.fabric8.tooling.testing", "pax-exam-karaf", MavenUtils.getArtifactVersion("io.fabric8.tooling.testing", "pax-exam-karaf")),
+                mavenBundle("org.jboss.fuse.itests", "esb-itests-common", MavenUtils.getArtifactVersion("org.jboss.fuse.itests", "esb-itests-common")),
+                wrappedBundle(mavenBundle("io.fabric8.itests", "fabric-itests-common", MavenUtils.getArtifactVersion("io.fabric8.itests", "fabric-itests-common"))),
    		};
    	}
 
